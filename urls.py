9e10bbc6979157cc1665c5a4af7c1412af5c9592
Yuri D. Lensky <ydlensky@gmail.com>
2012-07-28 14:41:10 -0400

    Cleaned up code to avoid dependence on containing folder.

--- urls.py
+++ urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 # Root-level urls
 urlpatterns = patterns(
-    'chiphi_betachapter.views',
-    url(r'^$', 'home', name="home"),
-    url(r'^contact/$', 'contact', name="contact"),
-    url(r'^house/$', 'house', name="house"),
+    'views',
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name="contact"),
+    url(r'^house/$', TemplateView.as_view(template_name='house.html'), name="house"),
 )
 
 # Admin-related urls
--- views.py
+++ views.py
@@ -1,10 +1,2 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'index.html')
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def house(request):
-    return render(request, 'house.html')
